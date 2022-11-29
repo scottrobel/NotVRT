19ad81562a196e9a38eaa2f5cd33551a60770b49
legendaryphoenixicarus <legendary.phoenix.icarus@gmail.com>
2022-11-29 03:34:55 -0500

    withdraw functionality has been implemented

--- contracts/StakeVRT.sol
+++ contracts/StakeVRT.sol
@@ -27,10 +27,17 @@ contract StakeVRT is Ownable {
 
     address public vrt;
     IERC20 iVrt;
+    
+    /**
+     * @notice We usually require to know who are all the stakeholders.
+     */
+    address[] internal stakeholders;
 
     mapping(address => Stake) private stakes;
 
     event Deposit(address user, uint256 amount, uint256 period, uint256 startTime);
+
+    event Withdraw(address user, uint256 amount, uint256 withdrawTime);
 
     constructor(address _vrt, address _rSnacks) {
         snacks = _rSnacks;
@@ -40,14 +47,69 @@ contract StakeVRT is Ownable {
         iVrt = IERC20(vrt);
     }
 
+    /**
+    * @notice The score factor can be set by only owner.
+    * @param _scoreFactor The score factor variable to set.
+    */
     function setScoreFactor(uint256 _scoreFactor) public onlyOwner {
         scoreFactor = _scoreFactor;
     }
     
+    /**
+    * @notice The reward factor can be set by only owner.
+    * @param _rewardFactor The reward factor variable to set.
+    */
     function setRewardFactor(uint256 _rewardFactor) public onlyOwner {
         rewardFactor = _rewardFactor;
     }
 
+    /**
+    * @notice A method to check if an address is a stakeholder.
+    * @param _address The address to verify.
+    * @return bool, uint256 Whether the address is a stakeholder,
+    * and if so its position in the stakeholders array.
+    */
+   function isStakeholder(address _address)
+       public
+       view
+       returns(bool, uint256)
+   {
+       for (uint256 s = 0; s < stakeholders.length; s += 1){
+           if (_address == stakeholders[s]) return (true, s);
+       }
+       return (false, 0);
+   }
+
+   /**
+    * @notice A method to add a stakeholder.
+    * @param _stakeholder The stakeholder to add.
+    */
+   function addStakeholder(address _stakeholder)
+       public
+   {
+       (bool _isStakeholder, ) = isStakeholder(_stakeholder);
+       if(!_isStakeholder) stakeholders.push(_stakeholder);
+   }
+
+   /**
+    * @notice A method to remove a stakeholder.
+    * @param _stakeholder The stakeholder to remove.
+    */
+   function removeStakeholder(address _stakeholder)
+       public
+   {
+       (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
+       if(_isStakeholder){
+           stakeholders[s] = stakeholders[stakeholders.length - 1];
+           stakeholders.pop();
+       }
+   }
+
+    /**
+    * @notice The main staking function.
+    * @param _amount The amount to stake.
+    * @param _period The period to stake.
+    */
     function deposit(uint256 _amount, uint256 _period) external {
         require(_amount > 0, "Should be not zero");
         require(_amount <= iVrt.balanceOf(msg.sender), "Balance is not enough");
@@ -57,10 +119,20 @@ contract StakeVRT is Ownable {
         Stake memory newStake = Stake(block.timestamp, block.timestamp + _period, _amount,_amount * _period / scoreFactor);
 
         stakes[msg.sender] = newStake;
+        addStakeholder(msg.sender);
         emit Deposit(msg.sender, _amount, _period, block.timestamp);
     }
 
-    function withdraw() external {}
+    function withdraw() external {
+        (bool _isStakeholder, uint256 s) = isStakeholder(msg.sender);
+        require(_isStakeholder, "Should be stakeholder to try withdraw");
+        require(block.timestamp >= stakes[msg.sender].endTime, "You can't withdraw before end time");
+        iVrt.transfer(msg.sender, stakes[msg.sender].amount);
+
+        emit Withdraw(msg.sender, stakes[msg.sender].amount, block.timestamp);
+        removeStakeholder(msg.sender);
+        delete(stakes[msg.sender]);
+    }
 
     function viewRewards(address _user) external returns (uint256) {}
 
