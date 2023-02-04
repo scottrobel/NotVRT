c1f6a1525d514c64368e6b3f7a5058d93d318d22
Scott Robel Jr <scott.robel.jr@gmail.com>
2023-02-04 16:52:21 -0500

    added comments

--- scripts/claimRewards.js
+++ scripts/claimRewards.js
@@ -9,10 +9,13 @@ async function recordStakeWallets() {
   let { data, error } = await supabase.from('users').select('key')
   let deposits = await StakeVRTContract.queryFilter(depositEventFilter)
   let usersWhoStaked = data.map((user) => { return user['key'] });
+  //loops though the deposits
   for (let deposit of deposits) {
     stakeUserAddress = deposit.args['user']
+    //checks if the depositer address was already added to the data base
     if (!usersWhoStaked.includes(stakeUserAddress)) {
       usersWhoStaked.push(stakeUserAddress)
+      //adds the new user to the database
       const { data, error } = await supabase
         .from('users')
         .insert({ key: stakeUserAddress })
@@ -26,23 +29,24 @@ const ClaimRewards = async () => {
   StakeVRTContract = await StakeVRTContract.attach(process.env.CONTRACT_ADDRESS)
   let lastClaimInfo = []
   let currentBlockTime = (await ethers.provider.getBlock("latest")).timestamp
-  // loops through deposit data from database to find
-  // the amount of blocks since a stake got its rewards claimed
-  // If it has never been claimed it gets the amount of blocks since
-  // the first deposit
+  //loops through depositers
   for (let { key } of data) {
-    stake = await StakeVRTContract.getStake(key)
-    if(stake[0].toNumber() > 0){
+    stake = await StakeVRTContract.getStake(key)//gets the current stake data from the contract
+    if(stake[0].toNumber() > 0){//checks if there is still money staked
       lastClaimTime = stake[3].toNumber()
       timeSinceLastClaim = currentBlockTime - lastClaimTime;
-      lastClaimInfo.push({ key, timeSinceLastClaim })
+      lastClaimInfo.push({ key, timeSinceLastClaim })//stores the key and the time since last claim in an array
     }
   }
+  //sorts the time since last claim
   let sortedLastClaimInfo = lastClaimInfo.sort((a, b)=> {return a.timeSinceLastClaim - b.timeSinceLastClaim}).reverse()
+  //selects the stakes that have not been claimed the longest
   let usersToClaim = sortedLastClaimInfo.slice(0, maxDailyRewardClaims)
   const [user] = await ethers.getSigners();
   for(const userToClaimIndex in usersToClaim){
+    //gets the key of the user to claim
     let userToClaim = usersToClaim[userToClaimIndex].key
+    //claims the rewards for that user
     result = await StakeVRTContract.connect(user).claimRewards(userToClaim)
   }
 }
