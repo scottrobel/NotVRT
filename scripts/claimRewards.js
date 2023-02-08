34dac5eebac4460da2fd7c9fd6964f9bdac0c0d6
Scott Robel Jr <scott.robel.jr@gmail.com>
2023-02-08 18:08:44 -0500

    update claimRewards.js

--- scripts/claimRewards.js
+++ scripts/claimRewards.js
@@ -5,26 +5,25 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 async function recordStakeWallets() {
   let StakeVRTContract = await ethers.getContractFactory("StakeVRT");
   StakeVRTContract = await StakeVRTContract.attach(process.env.CONTRACT_ADDRESS)
-  let depositEventFilter = StakeVRTContract.filters.ClaimRewards()
+  let depositEventFilter = StakeVRTContract.filters.Deposit()
   let { data, error } = await supabase.from('users').select('key')
   let deposits = await StakeVRTContract.queryFilter(depositEventFilter)
   let usersWhoStaked = data.map((user) => { return user['key'] });
   //loops though the deposits
-  for (let deposit of deposits) {
+  let usersToInsert = deposits.filter((deposit) => {
     stakeUserAddress = deposit.args['user']
-    //checks if the depositer address was already added to the data base
     if (!usersWhoStaked.includes(stakeUserAddress)) {
       usersWhoStaked.push(stakeUserAddress)
-      //adds the new user to the database
-      const { data, error } = await supabase
-        .from('users')
-        .insert({ key: stakeUserAddress })
+      return true;
     }
-  }
+  }).map((deposit) => {
+    stakeUserAddress = deposit.args['user']
+    return { key: stakeUserAddress };
+  })
+  await supabase.from('users').insert(usersToInsert)
 }
 const ClaimRewards = async () => {
   const { data, error } = await supabase.from('users').select('key')
-  const maxDailyRewardClaims = process.env.autoClaimsPerDay
   let StakeVRTContract = await ethers.getContractFactory("StakeVRT");
   StakeVRTContract = await StakeVRTContract.attach(process.env.CONTRACT_ADDRESS)
   let lastClaimInfo = []
@@ -32,23 +31,24 @@ const ClaimRewards = async () => {
   //loops through depositers
   for (let { key } of data) {
     stake = await StakeVRTContract.getStake(key)//gets the current stake data from the contract
-    if(stake[0].toNumber() > 0){//checks if there is still money staked
+    if (stake[0].gt(0)) {//checks if there is still money staked
       lastClaimTime = stake[3].toNumber()
       timeSinceLastClaim = currentBlockTime - lastClaimTime;
       lastClaimInfo.push({ key, timeSinceLastClaim })//stores the key and the time since last claim in an array
     }
   }
   //sorts stakes by the amount of time since last claim
-  let sortedLastClaimInfo = lastClaimInfo.sort((a, b)=> {return a.timeSinceLastClaim - b.timeSinceLastClaim}).reverse()
+  let sortedLastClaimInfo = lastClaimInfo.sort((a, b) => { return a.timeSinceLastClaim - b.timeSinceLastClaim }).reverse()
+  console.log(sortedLastClaimInfo)
   //selects the stakes that have not been claimed the longest
-  let usersToClaim = sortedLastClaimInfo.slice(0, maxDailyRewardClaims)
+  let userToClaim = sortedLastClaimInfo[0];
   const [user] = await ethers.getSigners();
-  for(const userToClaimIndex in usersToClaim){
-    //gets the key of the user to claim
-    let userToClaim = usersToClaim[userToClaimIndex].key
-    //claims the rewards for that user
-    result = await StakeVRTContract.connect(user).claimRewards(userToClaim)
-  }
+  //gets the key of the user to claim
+  let userToClaimKey = userToClaim.key
+  //claims the rewards for that user
+  console.log(userToClaimKey)
+  result = await StakeVRTContract.connect(user).claimRewards(userToClaimKey)
+  result.wait(1)
 }
 const main = async () => {
   await recordStakeWallets()
